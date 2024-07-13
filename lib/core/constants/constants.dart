b1d95c5a7041738ed9a44c52157223cf6b17d30d
A   S   H   I   S   H <96686068+PiPanther@users.noreply.github.com>
2024-07-13 16:23:36 +0530

    "Additions : Models"

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,4 +2,9 @@ class Constants {
   static const logoPath = 'assets/images/logo.png';
   static const googlePath = 'assets/images/google.png';
   static const loginEmotePath = 'assets/images/loginEmote.png';
+  static const pfp =
+      'https://i.pinimg.com/236x/bf/f5/d0/bff5d074d399bdfec6071e9168398406.jpg';
+  static const banner =
+      'https://t4.ftcdn.net/jpg/04/95/28/65/360_F_495286577_rpsT2Shmr6g81hOhGXALhxWOfx1vOQBa.jpg';
+      
 }
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit/core/constants/constants.dart';
 import 'package:reddit/core/providers/firebase_providers.dart';
+import 'package:reddit/models/user_model.dart';
 
 final authRepositoryProvider = Provider((ref) => AuthRepository(
     firestore: ref.read(fireStoreProvider),
@@ -32,6 +34,15 @@ class AuthRepository {
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
       print(userCredential.user?.email ?? "");
+
+      UserModel userModel = UserModel(
+          name: userCredential.user?.displayName ?? "No Name",
+          profilePic: userCredential.user?.photoURL ?? Constants.pfp,
+          banner: Constants.banner,
+          uid: userCredential.user!.uid,
+          isAuthenticated: true,
+          karma: 0,
+          awards: []);
     } catch (E) {
       print(E.toString());
     }
