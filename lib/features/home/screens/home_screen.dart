dc1d399400c2cdae479c6300644441841d2d7aa5
A   S   H   I   S   H <96686068+PiPanther@users.noreply.github.com>
2024-07-14 23:21:30 +0530

    "Additions : Logout Functionality"

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -3,12 +3,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit/features/auth/controllers/auth_controller.dart';
 import 'package:reddit/features/home/delegates/search_community_delegate.dart';
 import 'package:reddit/features/home/drawers/community_list_drawer.dart';
+import 'package:reddit/features/home/drawers/profile_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   void displayDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
+  }
+
+  void displayEndDrawer(BuildContext context) {
+    Scaffold.of(context).openEndDrawer();
   }
 
   @override
@@ -33,15 +38,20 @@ class HomeScreen extends ConsumerWidget {
                     delegate: SearchCommunityDelegate(ref: ref));
               },
               icon: const Icon(Icons.search)),
-          IconButton(
-            onPressed: () {},
-            icon: CircleAvatar(
-              backgroundImage: NetworkImage(user!.profilePic),
-            ),
-          )
+          Builder(builder: (context) {
+            return IconButton(
+              onPressed: () {
+                displayEndDrawer(context);
+              },
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(user!.profilePic),
+              ),
+            );
+          })
         ],
       ),
       drawer: const CommunityListDrawer(),
+      endDrawer: const ProfileDrawer(),
     );
   }
 }
